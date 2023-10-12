2a5a90c3f7eaeaa7f1aac56cdaf4b2c6a387e0c8
sori-k <vrti0913@naver.com>
2023-10-12 18:04:27 +0900

    ver02

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db'); //db import (같은 폴더 내에 없으니까 .. 으로 나가서)
+var multer = require('multer');
+
+// 업로드함수
+var upload = multer({
+    storage:multer.diskStorage({
+        destination:(req, file, done) => {
+            done(null, './public/upload/photo') // 현재 프로젝트의 퍼블릭에 업로드의 포토 (경로)
+        },
+        filename:(req, file, done) => { // 파일 이름 지정
+            var fileName = Date.now() + ".jpg";
+            done(null, fileName);
+        }
+    })
+});
 
 /* 사용자 목록 페이지로 이동 */
 router.get('/', function(req, res, next) {
@@ -47,20 +61,81 @@ router.get('/insert', function(req, res){
 });
 
 // 회원가입
-router.post('/insert', function(req, res){ //post의 action으로 가는데 submit 될때, 실행
+router.post('/insert', function(req, res){
     const uid=req.body.uid;
     const upass=req.body.upass;
     const uname=req.body.uname;
     const phone=req.body.phone;
     const address1=req.body.address1;
     const address2=req.body.address2;
-    console.log(uid,upass,uname,phone,address1,address2);
 
-    const sql="insert into users(uid, upass, uname, phone, address1, address2) values(?, ?, ?, ?, ?, ?)";
+    //console.log(uid, upass, uname, phone, address1, address2);
+
+    const sql='insert into users(uid, upass, uname, phone, address1, address2) values(?, ?, ?, ?, ?, ?)';
 
     //sql 실행
     db.get().query(sql, [uid, upass, uname, phone, address1, address2], function(err, rows){
         res.redirect('/users/login') //회원가입후 로그인 페이지로 이동 -> 데이터베이스에 저장된다.
     });
 });
+
+// 마이페이지로 이동
+router.get('/mypage', function(req, res){ //params로 받을 때) /:uid 넣기
+    const uid=req.query.uid; // params로 받을 때) const uid=req.params.uid;
+    const sql='select * from users where uid=?';
+    db.get().query(sql, [uid], function(err, rows){
+        //console.log('.............', uid);
+        //console.log(rows[0]);
+        res.render('index', {title:'마이페이지', pageName:'users/mypage.ejs', user:rows[0]})
+    }); 
+});
+
+// 수정페이지로 이동
+router.get('/update', function(req, res){
+    const uid=req.query.uid;
+    const sql='select * from users where uid=?';
+    db.get().query(sql, [uid], function(err, rows){
+        //console.log('.............', uid);
+        console.log(rows[0]);
+        res.render('index', {title:'정보수정', pageName:'users/update.ejs', user:rows[0]})
+    });
+});
+
+// 정보수정
+router.post('/update', upload.single('file'), function(req, res){
+    const uid=req.body.uid;
+    const uname=req.body.uname;
+    const phone=req.body.phone;
+    const address1=req.body.address1;
+    const address2=req.body.address2;
+    //console.log(uid,uname,phone,address1,address2);
+
+    let photo = req.body.photo; //기존 사진을
+    if(req.file) photo = req.file.filename; //바뀐 파일이 있으면 새롭게
+    console.log('photo....', photo);
+
+    //db에 업데이트
+    const sql='update users set uname=?, phone=?, address1=?, address2=?, photo=? where uid=?';
+    db.get().query(sql, [uname, phone, address1, address2, photo, uid], function(err, rows){
+        if(err) console.log(err); // 에러가 있으면 에러 출력 아니면 수정 후 마이 페이지로 이동
+        res.redirect('/users/mypage?uid=' + uid);
+    });
+});
+
+// 비밀번호변경 페이지로 이동
+router.get('/change', function(req, res){
+    res.render('index', {title:'비밀번호 변경', pageName:'users/change.ejs'})
+});
+
+// 비밀번호 변경
+router.post("/change", function(req, res){
+    const uid=req.body.uid;
+    const upass=req.body.npass;
+    const sql='update users set upass=? where uid=?';
+    db.get().query(sql, [upass, uid], function(err, rows){
+        if(err) console.log(err);
+        res.sendStatus(200); // 잘 실행되었는지 코드 보내는 거 200 -> ok
+    });
+});
+
 module.exports = router;
